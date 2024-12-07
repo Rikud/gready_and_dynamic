dce034b020c271f992adabebce7ab62670e96cf2
Ivan Nemshilov <i.nemshilov@corp.mail.ru>
2024-12-07 09:06:20 +0300

    fibonacci: memoization and tabulation

--- dynamic/fibonacci/main.go
+++ dynamic/fibonacci/main.go
@@ -12,6 +12,30 @@ func fib(n int) int {
 	return result
 }
 
+// Fibonacci with tabulation
+func fibTab(n int) int {
+	var result int
+
+	// TODO: implement
+
+	return result
+}
+
+// Fibonacci with memoization
+func fibMemo(n int, memo map[int]int) int {
+	// TODO: implement
+
+	return memo[n]
+}
+
+// Naive recursive Fibonacci
+func fibNaive(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fibNaive(n-1) + fibNaive(n-2)
+}
+
 func main() {
 	n := 10
 	fmt.Printf("Fibonacci number F(%d) = %d\n", n, fib(n))
